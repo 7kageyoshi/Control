b5c83e6b056f254450a28fd35a11dfeaea7de891
Maks Fedyunev <cmaksf@mail.ru>
2023-12-25 06:50:41 +0300

    решение

---
+++ Reshenie/Program.cs
@@ -0,0 +1,40 @@
+﻿
+string[] inputArray = { "Hello", "2", "world", ":-)" };
+
+string[] resultArray = FilterArray(inputArray);
+
+Console.WriteLine("Результат:");
+foreach (string str in resultArray)
+{
+    Console.Write($"{str} ");
+}
+
+Console.ReadLine();
+
+
+static string[] FilterArray(string[] inputArray)
+{
+    int count = 0;
+    foreach (string str in inputArray)
+    {
+        if (str.Length <= 3)
+        {
+            count++;
+        }
+    }
+
+    string[] resultArray = new string[count];
+
+    int index = 0;
+    foreach (string str in inputArray)
+    {
+        if (str.Length <= 3)
+        {
+            resultArray[index++] = str;
+        }
+    }
+
+    return resultArray;
+}
+
+
